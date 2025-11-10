de4ba181f47e90fbb920d287e498eea58bac476b
c2ordben <c2ordben@vasvari.org>
2025-11-10 09:37:26 +0100

    teszt6

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class TestOsszeadas(unittest.TestCase):
 
     def test_two_positive(self):
         fgv = osszead(1, 2
-        self.assertGreater(fgv, 3)
+        self.assertGreater(fgv, 3
 
     def test_two_zero(self):
         fgv = osszead(0,0)
